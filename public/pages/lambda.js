cee640e2b0bfc736e9e7234fd52dcad2af9ff006
Jean Helou <jhe@codamens.fr>
2019-10-03 21:17:19 +0200

    page which verifies ICU support

--- public/pages/lambda.js
+++ public/pages/lambda.js
@@ -1,10 +1,30 @@
 import Head from 'next/head';
+const Lambda = ({ value }) => {
+  const hasFullICU = () => {
+    try {
+      const january = new Date(9e8);
+      const spanish = new Intl.DateTimeFormat('es', { month: 'long' });
+      return spanish.format(january) === 'enero';
+    } catch (err) {
+      return false;
+    }
+  };
 
-const Lambda = ({value})=>
-(<>
-  <Head><title>Next.js + Lambda page</title></Head>
-  <p>this is a lamda because it has a getInitialProps, value : {value}</p>
-</>)
-
-Lambda.getInitialProps = ()=>{return {value:42};}
+  console.log('has full icu', hasFullICU());
+  return (
+    <>
+      <Head>
+        <title>Next.js + Lambda page</title>
+      </Head>
+      <p>this is a lamda because it has a getInitialProps, value : {value}</p>
+      <p>hasFullICU: {JSON.stringify(hasFullICU())}</p>
+    </>
+  );
+};
+Lambda.getInitialProps = () => {
+  if (typeof window === 'undefined') {
+    require('full-icu');
+  }
+  return { value: 42 };
+};
 export default Lambda;
