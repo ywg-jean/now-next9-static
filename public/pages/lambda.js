80751c4ba71e4659ba4efdddb04ad5625a80cb5a
Jean Helou <jhe@codamens.fr>
2019-10-04 15:26:46 +0200

    bypass fsn path and filetrace restriction

--- public/pages/lambda.js
+++ public/pages/lambda.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import fs from 'fs';
+import path from 'path';
 const Lambda = ({ value }) => {
   const hasFullICU = () => {
     try {
@@ -22,9 +24,26 @@ const Lambda = ({ value }) => {
     </>
   );
 };
-Lambda.getInitialProps = () => {
+Lambda.getInitialProps = async () => {
   if (typeof window === 'undefined') {
     require('full-icu');
+    async function getFiles(dir) {
+      const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+      const files = dirents.map(dirent => {
+        const res = path.resolve(dir, dirent.name);
+        return dirent.isDirectory()
+          ? Promise.resolve(res + '/')
+          : Promise.resolve(res);
+      });
+      const arr = await Promise.all(files);
+      return Array.prototype.concat(...arr);
+    }
+
+    //const files = await getFiles(__filename);
+    //files.forEach(file => console.log(file));
+    try {
+      fs.readFileSync(path.join(__dirname, 'full-icu', 'somefile.data'));
+    } catch (error) {}
   }
   console.log('env');
   console.log(JSON.stringify(process.env, null, 2));
